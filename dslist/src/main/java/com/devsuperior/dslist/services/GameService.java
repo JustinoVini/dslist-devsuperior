af5f74e658c44b7ce6f95c9d037caf4a66774d2b
Vinicius Justino <visilva5690@hotmail.com>
2023-05-11 22:09:58 -0300

    Get game by id

--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,9 +3,13 @@ package com.devsuperior.dslist.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -15,10 +19,21 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
-    
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        if (id == null) {
+            throw new EntityNotFoundException("A entidade com o id " + id + " não foi encontrada");
+        }
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
     }
-    
+
 }
